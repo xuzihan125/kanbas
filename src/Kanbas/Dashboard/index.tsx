e85215efb267c200a17f2c436688a1eb1b0a358b
Zihan Xu <xuzihan125@163.com>
2024-02-14 01:09:14 -0800

    JavaScript and React.js Assignment 3

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -14,8 +14,9 @@ function Dashboard() {
                     {courses.map((course) => (
                         <div key={course._id} className="col" style={{width: 300}}>
                             <div className="card">
-                                <img src={`/images/${course.image}`} className="card-img-top"
-                                     style={{height: 150}}/>
+
+                                <img src={`/images/${course.image}`} className="card-img-top" 
+                                     style={{height: 150}} alt={""}/>
                                 <div className="card-body">
                                     <Link className="card-title" to={`/Kanbas/Courses/${course._id}/Home`}
                                           style={{textDecoration: "none", color: "navy", fontWeight: "bold"}}>
